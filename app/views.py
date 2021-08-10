849c2ee9f1f42afc3514320cee5a31e563a38984
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-10 08:52:55 +0300

    Import flask and app to views.py

---
+++ app/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app
+from .request import get_data
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    popular_news = get_data()
+    print(popular_news)
+    title = 'NEWSROOM'
+    return render_template('index.html', title = title,popular=popular_news)
